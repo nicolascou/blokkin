e381f1d0870fc0974018601ce5bdb215e7a678cb
nicolascou <nicolascousillas1@gmail.com>
2022-06-01 12:21:06 +0200

    icons fixed

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { getPostsByName, searchUpdated } from '../actions/search';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'; 
 
 const Search = () => {
 
     const dispatch = useDispatch();
     const handleInputChange = ({target}) => {
         dispatch( searchUpdated( target.value ) );
-        dispatch( getPostsByName( target.value ) )
+        dispatch( getPostsByName( target.value ) );
     }
 
     return (
         <div className='bg-white h-12 w-full p-4 rounded-xl mb-8 flex items-center border-4 border-black'>
-            <i className='fa-solid fa-magnifying-glass inline-block'></i>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className='inline-block h-5 w-5' />
             <input type="text" onChange={ handleInputChange } name='search' className='mx-3 outline-none bg-transparent w-full font-bold' placeholder='Buscar...' />
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,6 @@ export default function Home({ posts }) {
                 <meta name='description' content='Blog en español sobre programación. Explicaciones sobre conceptos teóricos de la programación y nuevas tecnologías' />
 
                 <link rel="icon" href="/img/favicon.ico" />
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-
             </Head>
             <div className="container mx-auto px-10 mb-8 relative">
                 <div className='xl:hidden'>
@@ -24,17 +22,17 @@ export default function Home({ posts }) {
                 <div className='grid grid-cols-1 xl:grid-cols-12 xl:gap-10'>
                     {
                         (search !== '') ?
-                        <div className='xl:col-span-9 col-span-1 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 h-fit'> 
+                        <div className='xl:col-span-9 col-span-1 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 h-fit'> 
                             {
                                 cardSearched.map( (post) => <PostCard post={post.node} key={post.node.title} /> )
                             }
                         </div> 
                         :
-                        <div className='xl:col-span-9 col-span-1 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 h-fit'>
+                        <div className='xl:col-span-9 col-span-1 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 h-fit'>
                             {posts.map( (post) => <PostCard post={post.node} key={post.node.title} />)}
                         </div>
                     }
-                    <div className='xl:col-span-3 xl:block hidden'>
+                    <div className='xl:col-span-3 xl:block'>
                         <div className='relative xl:sticky top-8'>
                             <Search />
                             <Categories />
